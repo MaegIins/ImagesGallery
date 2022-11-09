eb3dc8d11ba43485db40e11ed4853c923b2ca429
Tristan <62724143+MaegIins@users.noreply.github.com>
2022-11-09 10:43:42 +0100

    popup login

---
+++ public/data/scripts/popup.js
@@ -0,0 +1,7 @@
+document.getElementById("loginBut").onclick = function() {
+    document.getElementById("popup").style.display = "block";
+}
+
+document.getElementById("cancelLogin").onclick = function () {
+    document.getElementById("popup").style.display = "none";
+}
