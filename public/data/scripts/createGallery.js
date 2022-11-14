cc90f733f03bad0551f1bbfab1a50a9691f72570
Quentin <quentin.dal-bon2@etu.univ-lorraine.fr>
2022-11-14 10:27:10 +0100

    update

--- public/data/scripts/createGallery.js
+++ public/data/scripts/createGallery.js
@@ -4,26 +4,39 @@ let buttonAddImage = document.getElementById("btAdd")
 let tags = document.getElementById("moreTags")
 let private = document.getElementById("priv")
 let public = document.getElementById("publ")
-let textUser = document.getElementById("newuser")
-let btnplus = document.getElementById("btnplus")
-let label = document.getElementById("labelUser")
 
 
 
-buttonAddImage.addEventListener("click", function(evt){
+
+buttonAddImage.addEventListener("click", function (evt) {
     let html = '<label for="img">Image Location : </label> <div id="divimg"> <input type="file" id="img" name="img" placeholder="Image Location"><input type="button" value="+" name="add_img" id="btAdd"></div><label for="Tags">Tag of the image (only one): </label><input type="text" id="tags" name="tag" placeholder="Tag">'
     tags.innerHTML += html;
-;    console.log("sss")
+    ; console.log("sss")
 })
 
 
-private.addEventListener("click", function(evt) {
-    textUser.hidden = false;
-    btnplus.hidden = false;
-    label.hidden = false;
+private.addEventListener("click", function (evt) {
+    let count = 0;
+
+    if (count == 0) {
+        count = 1
+        tags.insertAdjacentHTML("afterend", '<label for="newuser" id="labelUser">Add a new user : </label> <div id="divuser"> <input type="text" id="newuser" name="user" placeholder="New User"> <input type="button" id="btnplus"value="+" name="add_username"> </div>')
+    } else {
+        let textUser = document.getElementById("newuser")
+        let btnplus = document.getElementById("btnplus")
+        let label = document.getElementById("labelUser")
+        textUser.hidden = true;
+        btnplus.hidden = true;
+        label.hidden = true;
+    }
 })
 
-public.addEventListener("click", function(evt) {
+public.addEventListener("click", function (evt) {
+
+    let textUser = document.getElementById("newuser")
+    let btnplus = document.getElementById("btnplus")
+    let label = document.getElementById("labelUser")
+
     textUser.hidden = true;
     btnplus.hidden = true;
     label.hidden = true;
