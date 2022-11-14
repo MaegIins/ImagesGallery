93b2b0011cc23db3db3d34a369cd7bdb5e5ac46e
Tristan <62724143+MaegIins@users.noreply.github.com>
2022-11-14 13:34:59 +0100

    réparerer create gallery

--- public/data/scripts/createGallery.js
+++ public/data/scripts/createGallery.js
@@ -2,8 +2,8 @@ console.log("script chargé")
 
 let buttonAddImage = document.getElementById("btAdd")
 let tags = document.getElementById("moreTags")
-let private = document.getElementById("priv")
-let public = document.getElementById("publ")
+let privatee = document.getElementById("priv")
+let publice = document.getElementById("publ")
 let adduser = document.getElementById("moreUser")
 let add = document.getElementById("newuser")
 let btnplus = document.getElementById("btnplus")
@@ -11,29 +11,37 @@ let label = document.getElementById("labelUser")
 let buttonAddUser = document.getElementById("btnplus");
 
 
-
-
-
+/**
+ * Function that add a new input for the tags after pressing the + button
+ */
 buttonAddImage.addEventListener("click", function (evt) {
-    let html = '<label for="img">Image Location : </label> <div id="divimg"> <input type="file" id="img" name="img" placeholder="Image Location"><input type="button" value="+" name="add_img" id="btAdd"></div><label for="Tags">Tag of the image (only one): </label><input type="text" id="tags" name="tag" placeholder="Tag">'
+    let html = '<label for="img">Image Location : </label> <div id="divimg"> <input type="file" id="img" name="img" placeholder="Image Location"></div><label for="Tags">Tag of the image (only one): </label><input type="text" id="tags" name="tag" placeholder="Tag">'
     tags.insertAdjacentHTML("afterend", html)
+
 })
 
+
+/**
+ * Function that add a new input for the users after pressing the + button
+ */
 buttonAddUser.addEventListener("click", function (evt) {
-    let html = '<label for="newuser" id="labelUser">Add a new user : </label> <div id="divuser"> <input type="text" id="newuser" name="user" placeholder="New User"> <input type="button" id="btnplus"value="+" name="add_username"> </div>'
-    tags.insertAdjacentHTML("afterend", html)
+    console.log("click")
+    let html = '<label for="newuser" id="labelUser">Add a new user : </label> <div id="divuser"> <input type="text" id="newuser" name="user" placeholder="New User"></div>'
+    adduser.insertAdjacentHTML("afterend", html)
 })
 
 //ajoute add user quand le bouton radio priavte est select 
-private.addEventListener("click", function (evt) {
+privatee.addEventListener("click", function (evt) {
     btnplus.hidden = false;
     add.hidden = false;
     label.hidden = false
 })
-//rend le menu add user invisible quand c'est en public
-public.addEventListener("click", function (evt) {
+//rend le menu add user invisible quand c'est en publice
+publice.addEventListener("click", function (evt) {
 
     add.hidden = true;
     btnplus.hidden = true;
     label.hidden = true;
 })
+
+
